c7a0de7b561b503fe551911ad5f526f8c5a1770e
diego0819 <66338758+diego0819@users.noreply.github.com>
2025-04-19 16:38:39 -0500

    db.py

--- app/db.py
+++ app/db.py
@@ -2,14 +2,14 @@ import psycopg2
 from flask import current_app
 
 def get_db_connection():
-    """Devuelve una conexión a la base de datos."""
+    
     try:
         conn = psycopg2.connect(
-            dbname="postgres",  # Reemplaza con el nombre de tu base de datos
-            user="postgres",    # Reemplaza con tu usuario de base de datos
-            password="",  # Reemplaza con tu contraseña de base de datos
-            host="localhost",       # Reemplaza con tu host de base de datos si no es local
-            port="5432"            # Reemplaza con tu puerto de base de datos si no es el predeterminado
+            dbname="postgres",  
+            user="postgres",    
+            password="",  
+            host="localhost",      
+            port="5432"            
         )
         return conn
     except psycopg2.Error as e:
@@ -17,7 +17,7 @@ def get_db_connection():
         return None
 
 def insert_data_batch(table_name, data):
-    """Inserta un lote de datos en la tabla especificada."""
+    
 
     if not data:
         return  # Nada que insertar
@@ -37,13 +37,13 @@ def insert_data_batch(table_name, data):
         else:
             raise ValueError(f"Nombre de tabla inválido: {table_name}")
 
-        # Construir la sentencia SQL INSERT
+        # sentencia SQL INSERT
         placeholders = ', '.join(['%s'] * len(columns))
         insert_query = f"INSERT INTO {table_name} ({', '.join(columns)}) VALUES ({placeholders})"
 
-        values_list = [tuple(row[col] for col in columns) for row in data]  # Crear una lista de tuplas
+        values_list = [tuple(row[col] for col in columns) for row in data]  
 
-        cursor.executemany(insert_query, values_list)  # Insertar múltiples filas eficientemente
+        cursor.executemany(insert_query, values_list)  
         conn.commit()
 
     except psycopg2.Error as e:
